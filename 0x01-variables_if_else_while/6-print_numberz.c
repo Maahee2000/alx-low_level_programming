e7cee3aa47c02e00d033585bf7be9456c081056c
maahee200 <yongzmahi99@gmail.com>
2022-04-09 04:58:07 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - program that print all sigle digit number of
+ * base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 18; i < 28; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
