b6fb554ccf8685c3a20a90948c1f82491fd8555a
maahee200 <yongzmahi99@gmail.com>
2022-04-10 02:02:21 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,10 +17,10 @@ int main(void)
 		c = 0;
 		while (c < 10)
 		{
-			if (d != c && d <c)
+			if (d != c && d < c)
 			{
 				putchar('0' + d);
-				putchar('0' + c ) ;
+				putchar('0' + c);
 
 				if (c + d != 17)
 				{
