f18c49ac004683155e1c5fae4adb2e8d0da4b7b8
maahee200 <yongzmahi99@gmail.com>
2022-04-10 01:08:14 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *
  * main - main block
  *
- * Return: 0
+ * Return: (Always 0)
  *
  */
 int main(void)
 
 {
-		int c;
-		int d;
-		int e = 0;
-			while (e < 10)
-			{
-				d = 0;
-				while (d < 10)
-				{
-					c = 0;
-					while (c < 10)
-					{
-					if (c != d && d != e && e < d && d < c)
-						{
-							putchar('0' + e);
-							putchar('0' + d);
-							putchar('0' + c);
+	int c;
+	int d;
+	int e = 0;
 
-						if (c + d + e != 9 + 8 + 7)
-							{
-								putchar(',');
-								putchar(' ');
-							}
-						}
-						c++;
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
 					}
-					d++;
 				}
-				e++;
+				c++;
 			}
-			putchar('\n');
-			return (0);
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
