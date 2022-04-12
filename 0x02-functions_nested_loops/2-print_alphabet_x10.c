ab4b76da6c4c37c28aeb82e0cc866fc6ab56c688
maahee200 <yongzmahi99@gmail.com>
2022-04-12 16:44:47 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - This program print alphabet,
+ * fr0m a to z 10x in lower case.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char a, b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar(a);
+	}
+	_putchar('\n')
+}
