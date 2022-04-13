79b433d7c66119d00a56254c89107828e0e4dc9f
maahee200 <yongzmahi99@gmail.com>
2022-04-12 18:06:03 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_sign - this function prints sign of a numberr.
+ *
+ * @n: character
+ *
+ * Return: 1 if n is greater than 0
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 1)
+	{
+		putchr('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
