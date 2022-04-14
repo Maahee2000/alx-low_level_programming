ffaed85c1d299d84dcd21a2d2ace5982fd493374
maahee200 <yongzmahi99@gmail.com>
2022-04-14 14:53:50 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * main - causes an infinite loop
+ *
+ * Return: (Always 0)
+ */
+int  main(void)
+{
+	int i;
+
+	printf("infinite loop incoming" : ("\n"));
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("infinit loo avoided! \\o/n\n");
+
+	return (0);
+}
