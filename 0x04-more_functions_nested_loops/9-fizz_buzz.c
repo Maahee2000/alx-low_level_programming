0ca14b5e36c4c16065bf25857f83e0e7cd66bd06
maahee200 <yongzmahi99@gmail.com>
2022-04-15 06:47:30 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main - print fiz buzz
+ * @void: no argument
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	printF("buzz");
+	printf("\n");
+	return (0);
+}
