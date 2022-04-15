1ea95fdb8a8fd9b7a181ad41cfd396e2c1e26082
maahee200 <yongzmahi99@gmail.com>
2022-04-15 10:46:32 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,30 @@
 #include"main.h"
 
 /**
- * print_diagonal - This function draws a diagonal line
- * @size: this is the size of the squre
+ * print_square - print square
+ * @size: input size
  *
- * Return: (void)
+ * Description: prints a square
+ * Return: Always (0)
  */
-void print_diagonal(int size)
+
+void print_square(int size)
 {
 	int i, j;
 
 	if (size <= 0)
-		_putchar('\n');
-	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
