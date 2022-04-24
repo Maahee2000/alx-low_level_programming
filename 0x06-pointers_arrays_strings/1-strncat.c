918b6e4cf1d7c907c94b5fd0fb2362de8a877e2a
maahee200 <yongzmahi99@gmail.com>
2022-04-24 00:13:17 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include"mai.h"
+
+/**
+ * _strncat - Function that concatenate two string
+ * @dest: input parameter string
+ * @src: input parameter string
+ * @n: times to interaction iterations
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != 0)
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != 0 && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
+}
