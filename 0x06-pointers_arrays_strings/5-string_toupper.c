32f674bfdc1f51f517e213530e52f3004ee2953e
maahee200 <yongzmahi99@gmail.com>
2022-04-24 05:59:45 -0700

    main

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,13 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
 		}
+		i++;
 	}
 	return (str);
 }
