95383f41ac7280162d584f97d555d8b2de62330f
maahee200 <yongzmahi99@gmail.com>
2022-04-24 05:57:13 -0700

    main

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,6 @@
 /**
  * string_toupper - changes lowercase to uppercase
  *
- *
  * @str: string to return.
  * Return: the string that change for uppercase
  */
@@ -11,13 +10,12 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
 		}
-		i++;
 	}
 	return (str);
 }
