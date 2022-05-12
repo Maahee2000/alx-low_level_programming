0bacd7d191298439a5d19916c1699c1c3fc24661
maahee200 <yongzmahi99@gmail.com>
2022-05-11 19:38:55 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,14 @@
  * @age: character string
  * @owner: character strin
  */
+
 typedef struct dog
+
 {
 	char *name;
 	float age;
 	char *owner;
-}
-dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
